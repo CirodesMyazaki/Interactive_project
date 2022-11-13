e1b13dd3dfbf980c17cb75cc55e00066433e8b96
CirodesMyazaki <118015963+CirodesMyazaki@users.noreply.github.com>
2022-11-13 19:03:10 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,462 @@
+let topTitleDiv = "<h4><b>COVID-19 AND LABOR MARKET INEQUALITIES</h4>";
+
+let titleDiv =
+  "<h1>Working from home makes you happier (and more productive). But not everyone can do it</h1>";
+
+let bylineDiv = "";
+
+
+let descriptionDiv =
+  '<p>Since 2020, Covid-19 lockdowns have forced many European citizens to start working from home. This is one of the longest lasting consequences of the pandemic, and one of the biggest shifts in labor market arrangements in the past few decades. Since the beginning of the pandemic, the share of remote workers has more than doubled, from 5.5 per cent in 2019 to 15.3 per cent in 2022, <a href="https://ec.europa.eu/eurostat/web/products-eurostat-news/-/ddn-20221108-1" taget="_blank">according to figures published by Eurostat</a>.</p>' +
+  '<p>Yet, this is not true for everyone. While aggregate mobility data suggests that workplace mobility is returning close to pre-pandemic levels, regional data shows that large differences exist within countries: in rich regions – where high-skilled workers are prevalent – mobility is still more than 25 per cent below pre pandemic levels; poorer regions have seen a return to higher mobility levels. </p>' +
+  '<p>In short, the richest workers aren’t going back to the workplace.</p>' +
+  '<div style="max-width:1200px; margin-left:auto; margin-right:auto; text-align: center"><iframe width=100% height=100% src="images/ai2html-bars.html" frameborder="0" scrolling="no" onload="resizeIframe(this)" /></iframe></div>' +
+  '<p style="text-align:center">Scroll to continue<br>▼</p>';
+
+let footerDiv =
+  '<p>This story is based on Google LLC <a href="https://www.google.com/covid19/mobility/" taget="_blank">Covid-19 Community Mobility Reports</a>, <a href="https://www.mapbox.com/about/maps/" target="_blank">© Mapbox</a>';
+
+let divChapter1 =
+  "<h2>APRIL 2020</h2>" +
+  "<p>At the beginning of the pandemic, governments across Europe imposed nation-wide lockdowns to stop the spread of the virus. Workers had to adapt, and started working from home. On average, mobility to and from the workplace collapsed by <b style=background:#5f0202;color:#ffff;>more than 50 per cent</b> across Europe in April 2020, as Google mobility data shows. The largest drop was recorded in Spain with workplace mobility shrinking just shy of <b style=color:#5f0202;>70 per cent</b> over the same period.</p>";
+
+let divChapter2 =
+  "<h2>APRIL 2021</h2>" +
+  "<p> After a year, lockdowns had all been lifted and vaccination campaigns reached their final stage. Yet, workplace mobility still remained, on average, <b style=background:#ffb4a1;color:#ffff;>25 per cent</b> below pre-pandemic levels. In France and the UK, mobility was down <b style=color:#e64e53;>more than 30 per cent</b>.";
+
+let divChapter3 =
+  "<h2>APRIL 2022</h2>" +
+  "<p> It took another year, the end of vaccination campaigns, and most restrictions be lifted for workplace mobility to near pre-pandemic levels. In countries such as <b style=background:#85e5d3;>Poland, Lithuania, and Hungary </b>, mobility increased beyond pre-pandemic levels.";
+
+let divChapter4 =
+  "<h2>SEPTEMBER 2022</h2>" +
+  "<p>Latest monthly data show mobility to and from the workplace being on average just 8 percentage points below pre-pandemic levels across Europe. A sign that, finally, things are almost back to normal.</b>";
+
+let divChapter5 =
+  '<iframe width=100% height=100% src="images/ai2html-lines.html" frameborder="0" scrolling="no" onload="resizeIframe(this)" /></iframe>';
+
+  let divChapter6 =
+"<p style='max-width:600px; margin-left:auto; margin-right:auto'; text-align: center>The path to recovery, however, has not been the same for everyone.</p>";
+
+let divChapter7 =
+  "<p style='text-align: center'> Looking beyond national aggregates, sub-national data shows vast differences in mobility patterns across Europe. The map below illustrates mobility data for 2022: the <b style=color:#aa1829;>redder the region</b>, the <b style=color:#aa1829;>furthest the mobility</b> from pre Covid-19 trends.</p>";
+ 
+let divChapter8=
+  "<p>In France, for example, mobility was 14 percentage points below pre-pandemic levels in 2022. However, mobility was still more than 25 per cent below Covid-19 levels in the Paris metropolitan area. Moreover, this difference persists: looking at the past two months, mobility in the  Île-de-France area  was 31 points below pre-pandemic levels versus a French-wide metric of just 15 percentage points.</p>";
+
+let divChapter9=
+  "<p>This is true for other capital cities as well. In London, mobility in 2022 was still 30 points below February 2020, with some boroughs as low as 35 percentage points (e.g. City of London, Islington, and Richmond upon Thames). By contrast, the UK average was 23 per cent below baseline over the same period.</p> ";
+
+let divChapter10=
+"<p>In Madrid, mobility in 2022 was still 20 points below pre-pandemic levels whereas, at a national level, mobility in Spain was 15 points below baseline.</p>";
+
+let divChapter11=
+"<p> The trend is clear: across the whole of Europe, people living in metropolitan areas aren’t going back to the workplace as much as citizens living in less central areas.</p>"+
+'<iframe width=100% height=100% src="images/ai2html-dotplot.html" frameborder="0" scrolling="no" onload="resizeIframe(this)" /></iframe>';
+
+let divChapter12 =
+  "<p style='max-width:600px; margin-left:auto; margin-right:auto'>Regions where mobility is lowest have something in common: they are the richest in Europe. When comparing mobility gaps with regional GDP per capita a clear relationship emerges: higher GDP correlates with lower mobility vis-à-vis pre pandemic levels. This is also true when looking at income proxies, such as skill levels. Regions where high-skilled jobs are more abundant see lower mobility data.</p> " +
+  '<iframe width=100% height=100% src="images/ai2html-scatterplot.html" frameborder="0" scrolling="no" onload="resizeIframe(this)" /></iframe>'+
+  "<p style='max-width:600px; margin-left:auto; margin-right:auto'>Recent economic literature on <a href=https://www.economist.com/finance-and-economics/2021/10/30/remote-first-work-is-taking-over-the-rich-world> the inequality of working from home </a> confirm that income and skill levels affect worker mobility. The possibility of home working is positively correlated with high-paying occupations, elevated educational attainment, formal employment status and household wealth. <a href=https://econpapers.repec.org/paper/bdiopques/qef_5f606_5f21.htm>Research by economists Francesca Carta and Marta De Philippis shows</a>,  for example, that among the jobs performed by the 20 per cent top income earners in Italy, jobs that can be done at home are 30 percentage points higher than those that can be performed by the bottom 20 per cent of earners. The richer the worker, the higher the chances of working from home. This relationship stems from the particular configuration of labor markets in richer regions: wealthier regions exhibit larger shares of self-employed people, who are better placed to work from home. Moreover, better educated workers with <a href=https://bfi.uchicago.edu/wp-content/uploads/BFI_White-Paper_Dingel_Neiman_3.2020.pdf> higher wage premia tend to live in richer regions and large cities</a>. Both labor market traits indicate that there is a high concentration of workers with greater potential of working from home in those areas.</p>"+
+  "<p style='max-width:600px; margin-left:auto; margin-right:auto'>Working from home arrangements improve happiness and productivity too. Nicholas Bloom, a professor of economics at Stanford University, <a href=https://bfi.uchicago.edu/wp-content/uploads/BFI_White-Paper_Dingel_Neiman_3.2020.pdf>shows</a> that employees working from home (Friday or Monday especially) are significantly happier than their peers. Their productivity is also marginally higher. </p>"+
+  "<p style='max-width:600px; margin-left:auto; margin-right:auto'>Working from home has clear positive impacts. Yet not everyone can afford it. Poorer regions – and workers – have more limited access to it. The covid-induced inequality of working from home is here to stay.</p>";
+
+  var config = {
+  style: "mapbox://styles/ciromiyazaki/clacuzl50000n14lkwcmx1lcz",
+  accessToken: "pk.eyJ1IjoiY2lyb21peWF6YWtpIiwiYSI6ImNsYWN1dGJvcjBjYnUzd295cjVncHkzYjYifQ.VexCiaTMPPLQo6BlW8mTDQ",
+  showMarkers: false,
+  markerColor: "",
+  theme: "light",
+  use3dTerrain: false,
+  topTitle: topTitleDiv,
+  title: titleDiv,
+  subtitle: "",
+  byline: bylineDiv,
+  description: descriptionDiv,
+  footer: footerDiv,
+  chapters: [
+    {
+      id: "chapter1",
+      alignment: "left",
+      hidden: false,
+      chapterDiv: divChapter1,
+      location: {
+        center: [10, 45.725],
+        zoom: 4,
+        zoomSmall: 9,
+        pitch: 0,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "apr_20",
+          opacity: 1,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "apr_20",
+          opacity: 0,
+          duration: 300,
+        },
+      ],
+    },
+    {
+      id: "chapter2",
+      alignment: "left",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter2,
+      location: {
+        center: [10, 45.725],
+        zoom: 4,
+        zoomSmall: 9,
+        pitch: 0,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "apr_21",
+          opacity: 1,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "apr_21",
+          opacity: 0,
+        },
+      ],
+    },
+    {
+      id: "chapter3",
+      alignment: "left",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter3,
+      location: {
+        center: [10, 45.725],
+        zoom: 4,
+        zoomSmall: 9,
+        pitch: 0,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "apr_22",
+          opacity: 1,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "apr_22",
+          opacity: 0,
+        },
+      ],
+    },
+    {
+      id: "chapter4",
+      alignment: "left",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter4,
+      location: {
+        center: [10, 45.725],
+        zoom: 4,
+        zoomSmall: 9,
+        pitch: 0,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "sett_22",
+          opacity: 1,
+          duration: 300,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "sett_22",
+          opacity: 0,
+          duration: 300,
+        },
+      ],
+    },
+    {
+      id: "chapter5",
+      alignment: "full",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter5,
+      location: {
+        center: [10, 45.725],
+        zoom: 4,
+        zoomSmall: 9,
+        pitch: 0,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [],
+      onChapterExit: [],
+    },
+    {
+      id: "chapter6",
+      alignment: "center",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter6,
+      location: {
+        center: [10, 45.725],
+        zoom: 4,
+        zoomSmall: 9,
+        pitch: 0,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: "",
+      onChapterExit: "",
+    },
+    {
+      id: "chapter7",
+      alignment: "center",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter7,
+      location: {
+        center: [10, 45.725],
+        zoom: 4,
+        zoomSmall: 9,
+        pitch: 0,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "europe_nuts_clean",
+          opacity: 1,
+          duration: 300,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "europe_nuts_clean",
+          opacity: 0,
+          duration: 300,
+        },
+      ],
+    },
+    {
+      id: "chapter8",
+      alignment: "left",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter8,
+      location: {
+        center: [2.31, 47.725],
+        zoom: 6,
+        zoomSmall: 9,
+        pitch: 0,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [],
+      onChapterEnter: [
+        {
+          layer: "france",
+          opacity: 1,
+        },
+        {
+          layer: "countries",
+          opacity: 1,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "france",
+          opacity: 0,
+          duration: 300,
+        },
+        {
+          layer: "countries",
+          opacity: 0,
+          duration: 300,
+        },
+      ],
+    },
+    {
+      id: "chapter9",
+      alignment: "left",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter9,
+      location: {
+        center: [-0.1, 51.725],
+        zoom: 7,
+        zoomSmall: 9,
+        pitch: 31,
+        bearing: -40,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [],
+      onChapterEnter: [
+        {
+          layer: "europe_nuts_clean",
+          opacity: 1,
+          duration: 300,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "sett_22",
+          opacity: 0,
+          duration: 300,
+        },
+      ],
+    },
+    {
+      id: "chapter10",
+      alignment: "left",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter10,
+      location: {
+        center: [-3, 40.725],
+        zoom: 6.5,
+        zoomSmall: 9,
+        pitch: 0,
+        bearing: -3,
+        },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [],
+      onChapterEnter: [
+        {
+          layer: "europe_nuts_clean",
+          opacity: 1,
+          duration: 300,
+        },
+        {
+          layer: "countries",
+          opacity: 1,
+          duration: 300,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "sett_22",
+          opacity: 0,
+          duration: 300,
+        },
+        {
+          layer: "countries",
+          opacity: 0,
+          duration: 300,
+        },
+      ],
+    },
+    {
+      id: "chapter11",
+      alignment: "left",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter11,
+      location: {
+        center: [10, 45.725],
+        zoom: 4,
+        pitch: 0,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [],
+      onChapterEnter: [
+        {
+          layer: "europe_nuts_clean",
+          opacity: 1,
+          duration: 300,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "sett_22",
+          opacity: 0,
+          duration: 300,
+        },
+      ],
+    },
+    {
+      id: "chapter12",
+      alignment: "full",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter12,
+      location: {
+        center: [10, 45.725],
+        zoom: 4,
+        zoomSmall: 9,
+        pitch: 0,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "white",
+          opacity: 0.7,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "white",
+          opacity: 0,
+        },
+      ],
+    },
+  ],
+};
